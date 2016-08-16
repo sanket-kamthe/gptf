b955c0123efcba9058f5e48f41e46b1562c3b3db
Blaine Rogers <blaine.rogers14@imperial.ac.uk>
2016-08-16 18:35:23 +0100

    Removed __test__

--- gptf/__init__.py
+++ gptf/__init__.py
@@ -1,12 +1,3 @@
 """Provides a GPflow / GPy like interface for distributed GPs."""
-from . import params, wrappedtf, parentable, transforms
 __author__ = "Blaine Rogers <br1314@ic.ac.uk>"
 __version__ = "0"
-
-# in aid of doctest discovery
-__test__ = \
-        { 'params': params
-        , 'wrappedtf': wrappedtf
-        , 'parentable': parentable
-        , 'transforms': transforms
-        }
