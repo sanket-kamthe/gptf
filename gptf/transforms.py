ed1074f89be1954d1c11470668999f7c051ff1de
Blaine Rogers <blaine.rogers14@imperial.ac.uk>
2016-08-23 12:39:53 +0100

    Transfrom is now an ABC; added @overrides

--- gptf/transforms.py
+++ gptf/transforms.py
@@ -1,11 +1,15 @@
 """Provides a number of transforms that can be used to constrain `Param`s."""
 from builtins import object
+from future.utils import with_metaclass
+
+from abc import ABCMeta, abstractmethod
 import inspect
 
 import numpy as np
 import tensorflow as tf
+from overrides import overrides
 
-class Transform(object):
+class Transform(with_metaclass(ABCMeta, object)):
     """A transform, used to constrain the optimisation of a `Param`.
 
     NB: Unless otherwise stated, attributes of transforms are read-only
@@ -13,36 +17,44 @@ class Transform(object):
     of a `Param`, assign a _new_ `Transform` to its `.transform` attribute.
 
     """
+    @abstractmethod
     def tf_forward(self, x):
         """Map from the free space to the variable space using TensorFlow."""
         NotImplemented
 
+    #@abstractmethod
     #def tf_backward(self, x):
     #    """Map from the variable space to the free space using TensorFlow."""
     #    NotImplemented
 
+    #@abstractmethod
     #def np_forward(self, x):
     #    """Map from the free space to the variable space using NumPy."""
     #    NotImplemented
 
+    @abstractmethod
     def np_backward(self, x):
         """Map from the variable to the free space using NumPy."""
         NotImplemented
     
 class Identity(Transform):
     @staticmethod
+    @overrides
     def tf_forward(x):
         return tf.identity(x)
 
+    #@overrides
     #@staticmethod
     #def tf_backward(x):
     #    return tf.identity(x)
 
+    #@overrides
     #@staticmethod
     #def np_forward(x):
     #    return x
 
     @staticmethod
+    @overrides
     def np_backward(x):
         return x
 
@@ -68,15 +80,19 @@ class Exp(Transform):
         """
         self._lower = lower
 
+    @overrides
     def tf_forward(self, x):
         return tf.exp(x) + self._lower
 
+    #@overrides
     #def tf_backward(self, x):
     #    return tf.log(x - self._lower)
 
+    #@overrides
     #def np_forward(self, x):
     #    return np.exp(x) + self._lower
 
+    @overrides
     def np_backward(self, y):
         return np.log(y - self._lower)
 
