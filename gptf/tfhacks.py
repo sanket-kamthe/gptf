29710036a58f2a3b9b58408dcd52411f746ab936
Blaine Rogers <blaine.rogers14@imperial.ac.uk>
2016-08-30 15:32:24 +0100

    eye now produces square matrix on int argument

--- gptf/tfhacks.py
+++ gptf/tfhacks.py
@@ -9,16 +9,24 @@ def eye(shape, dtype=tf.float64):
     """Constructs an eye matrix of the requested shape.
     
     Args:
-        shape ((int, int)): The shape to construct.
+        shape (int | (int, int)): The shape to construct. If `int`, a square
+            matrix with `shape` rows and `shape` columns will be constructed.
+            Else, a matrix with `shape[0]` rows and `shape[1]` columns will 
+            be constructed.
 
     Returns:
         (tf.Tensor): A matrix with the requested shape and 1s on the diagonal.
 
     """
     with tf.name_scope("gptf.tfhacks.eye") as scope:
-        n = tf.minimum(shape[0], shape[1])
-        identity = tf.diag(tf.ones([n], dtype=dtype))
-        padrows = tf.to_int32(shape[0]) - n
-        padcolumns = tf.to_int32(shape[1]) - n
-        paddings = [[0, padrows], [0, padcolumns]]
-        return tf.pad(identity, paddings, name=scope)
+        try:
+            assert len(shape) == 2  # raises TypeError if shape is int
+            n = tf.minimum(shape[0], shape[1])
+            identity = tf.diag(tf.ones([n], dtype=dtype))
+            padrows = tf.to_int32(shape[0]) - n
+            padcolumns = tf.to_int32(shape[1]) - n
+            paddings = [[0, padrows], [0, padcolumns]]
+            return tf.pad(identity, paddings, name=scope)
+        except TypeError:
+            # shape is int
+            return tf.diag(tf.ones([shape], dtype=dtype), name=scope)
